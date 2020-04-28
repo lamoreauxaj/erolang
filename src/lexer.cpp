6ce3fabf5f9f5ed48ce88ab628634441d70839f2
Anand Iyer <anandiyer1000@gmail.com>
2020-04-27 23:38:18 -0500

    implemented char_type and one_char

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,14 +2,28 @@
 #include <string>
 #include <vector>
 
-enum Kind { };
-
-Kind char_type() {
-
+TokenType char_type(char c) {
+    switch (c) {
+        case '\n': return NEWLINE;
+        case '\\': return BACKSLASH;
+        case ';': return SEMICOLON;
+        case '(': return LEFT_PAREN;
+        case ')': return RIGHT_PAREN;
+        case '[': return LEFT_BRACKET;
+        case ']': return RIGHT_BRACKET;
+        case '{': return LEFT_BRACE;
+        case '}': return RIGHT_BRACE;
+        case '=': return EQUALS;
+        case '#': return SINGLE_COMMENT;
+        case ',': return COMMA;
+        case '-': return APPLY;  
+        default: return IDENTIFIER;
+    }
 }
 
-bool one_char() {
-
+bool one_char(TokenType t) {
+    return (t == NEWLINE) | (t == BACKSLASH) | (t == SEMICOLON) | (t == LEFT_PAREN) | (t == RIGHT_PAREN) | 
+        (t == LEFT_BRACKET) | (t == RIGHT_BRACKET) | (t == LEFT_BRACE) | (t == RIGHT_BRACE) | (t == EQUALS) | (t == COMMA);
 }
 
 bool is_keyword() {
@@ -23,5 +37,5 @@ Token next_token() {
 vector<Token> tokenize(string program) {
     vector<Token> tokens;
 
-    
+
 }
