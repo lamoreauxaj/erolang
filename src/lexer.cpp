ef65102156745ba9ce9890ccdb64249eef536d47
Anand Iyer <anandiyer1000@gmail.com>
2020-04-27 23:16:17 -0500

    base functions

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,6 +2,26 @@
 #include <string>
 #include <vector>
 
+enum Kind { };
+
+Kind char_type() {
+
+}
+
+bool one_char() {
+
+}
+
+bool is_keyword() {
+
+}
+
+Token next_token() {
+
+}
+
 vector<Token> tokenize(string program) {
+    vector<Token> tokens;
+
     
 }
