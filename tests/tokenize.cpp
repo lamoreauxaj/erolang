09795887c26f5158125eee6dda517ebc65b3ebb9
Anand Iyer <anandiyer1000@gmail.com>
2020-04-28 14:25:38 -0500

    added test

--- tests/tokenize.cpp
+++ tests/tokenize.cpp
@@ -38,3 +38,27 @@ TEST(tokenize2, assignment2) {
     };
     checkTokens(tokens, exp);
 }
+
+TEST(tokenize3, assignment3) {
+    string program = "c= 2\n e =.5\n f=3.14\ng=1.4e5\n";
+    vector<Token> tokens = tokenize(program);
+    vector<Token> exp = {
+        Token(TokenType::IDENTIFIER, "c"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::REAL, "2"),
+        Token(TokenType::NEWLINE, "\n"),
+        Token(TokenType::IDENTIFIER, "e"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::REAL, ".5"),
+        Token(TokenType::NEWLINE, "\n"),
+        Token(TokenType::IDENTIFIER, "f"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::REAL, "3.14"),
+        Token(TokenType::NEWLINE, "\n"),
+        Token(TokenType::IDENTIFIER, "g"),
+        Token(TokenType::ASSIGN, "="),
+        Token(TokenType::REAL, "1.4e5"),
+        Token(TokenType::NEWLINE, "\n")
+    };
+    checkTokens(tokens, exp);
+}
