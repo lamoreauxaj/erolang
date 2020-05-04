ee8d312af59b96eaa76673a82e49cb65afccd62c
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2020-05-04 05:45:48 -0500

    output to tmp file

--- docs/server/functions/compile/compile.js
+++ docs/server/functions/compile/compile.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
     try {
         const subject = event.queryStringParameters.name || 'World'
         // const f = fs.readFileSync('build/src/Ero_run')
-        let res = execSync('./Ero_run 1.ero 1 --prod').toString()
+        let res = execSync('./Ero_run 1.ero /tmp/1 --prod').toString()
         // exec('ls', (error, stdout, stderr) => {
         //     return {
         //         statusCode: 200,
--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
 
         string command;
         if (prod)
-            command = "./g++ -static -o " + output_file + " ../../" + output_file + ".s ./variable.o ./geometry.o";
+            command = "./g++ -static -o " + output_file + " " + output_file + ".s ./variable.o ./geometry.o";
         else
             command = "g++ -static -o " + output_file + " " + output_file + ".s ../../src/lib/variable.cpp ../../src/lib/geometry.cpp";
         int rc = system(command.c_str());
