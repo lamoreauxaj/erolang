e84182de19f87598a054c030d2ea719f9eef5343
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2020-05-04 19:12:43 -0500

    test of deploy server

--- docs/server/server/index.js
+++ docs/server/server/index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world! I hope this works Anand!')
 })
 
 app.listen(PORT, () => {
