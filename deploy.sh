e0af4757b1b651ed6f583807ff59359fbb786a58
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2020-05-04 19:25:03 -0500

    chain commands in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cd docs/server
-npm install
-pip install awscli --user
-npm run build
-aws s3 sync --acl public-read --sse --delete .vuepress/dist s3://erolang.xyz
-cd ../..
-chmod 400 erolang.pem
+cd docs/server &&
+npm install &&
+pip install awscli --user &&
+npm run build &&
+aws s3 sync --acl public-read --sse --delete .vuepress/dist s3://erolang.xyz &&
+cd ../.. &&
+chmod 400 erolang.pem &&
 ssh -i "erolang.pem" -o "StrictHostKeyChecking no" ubuntu@ec2-54-196-169-10.compute-1.amazonaws.com <<EOF
-    cd pa
-    rm -rf build
-    rm -rf docs/server/node_modules
-    git pull
-    cd docs/server
-    npm install
-    cd ../../
-    mkdir build
-    cd build
-    cmake ../
-    pm2 restart 0
+    cd pa &&
+    rm -rf build &&
+    rm -rf docs/server/node_modules &&
+    git pull &&
+    cd docs/server &&
+    npm install &&
+    cd ../../ &&
+    mkdir build &&
+    cd build &&
+    cmake ../ &&
+    pm2 restart 0 &&
     exit
 EOF
