8b55effffdae55aac362d9a2db850ce0f1e2c095
Aaron Lamoreaux <lamoreauxaj@gmail.com>
2020-05-06 03:29:44 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ npm install &&
 pip install awscli --user &&
 npm run build &&
 aws s3 sync --acl public-read --sse --delete .vuepress/dist s3://erolang.xyz &&
+aws configure set preview.cloudfront true &&
+aws cloudfront create-invalidation --distribution-id E1XV2436GAQ6E5 --paths '/*' &&
 cd .. &&
 chmod 400 erolang.pem &&
 ssh -i "erolang.pem" -o "StrictHostKeyChecking no" ubuntu@ec2-54-196-169-10.compute-1.amazonaws.com <<EOF
